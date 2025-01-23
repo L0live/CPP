8f38a45a71be12712a86ba923bfc543c8e32deee
Yann Olive <yanolive@3F6.42angouleme.fr>
2025-01-23 14:58:26 +0100

    verif module_01

--- Module_01/ex01/main.cpp
+++ Module_01/ex01/main.cpp
@@ -1,20 +1,6 @@
 #include "Zombie.hpp"
 
-int	main(void)
-{
-	std::cout << "Zombie ex00 (ZombieHeap&Stack):" << std::endl;
-	Zombie	*foo;
-
-	std::cout << "\n# Birth and announce of \"Foo\" Zombie on the HEAP\n" << std::endl;
-	foo = newZombie("Foo");
-	foo->announce();
-
-	std::cout << "\n# Birth, announce and death of \"Roo\" Zombie on the STACK\n" << std::endl;
-	randomChump("Roo");
-	std::cout << "\n# Death of \"Foo\" Zombie\n" << std::endl;
-	delete foo;
-
-	std::cout << "\nZombie ex01 (ZombieHorde):\n" << std::endl;
+int	main(void) {
 	int	hordeNbr = 10;
 	Zombie	*horde = zombieHorde(hordeNbr, "Boo");
 
