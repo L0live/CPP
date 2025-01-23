1815203243f301d0fa09026ac77b5dd6509ff778
Yann Olive <yanolive@3F6.42angouleme.fr>
2025-01-23 15:25:34 +0100

    module_02 clean

--- Module_02/ex01/Fixed.cpp
+++ Module_02/ex01/Fixed.cpp
@@ -1,54 +1,46 @@
 #include "Fixed.hpp"
 
-Fixed::Fixed() : _rawBits(0) {
-    std::cout << "Default constructor called" << std::endl;
-}
+Fixed::Fixed() : _rawBits(0) {std::cout << "Default constructor called" << std::endl;}
 
 Fixed::Fixed(const int value) {
-    std::cout << "Int constructor called" << std::endl;
-    this->_rawBits = value << this->_fractionalBits;
+	std::cout << "Int constructor called" << std::endl;
+	this->_rawBits = value << this->_fractionalBits;
 }
 
 Fixed::Fixed(const float value) {
-    std::cout << "Float constructor called" << std::endl;
-    this->_rawBits = roundf(value * (1 << this->_fractionalBits));
+	std::cout << "Float constructor called" << std::endl;
+	this->_rawBits = roundf(value * (1 << this->_fractionalBits));
 }
 
 Fixed::Fixed(const Fixed &fixed) {
-    std::cout << "Copy constructor called" << std::endl;
-    *this = fixed;
+	std::cout << "Copy constructor called" << std::endl;
+	*this = fixed;
 }
 
-Fixed::~Fixed() {
-    std::cout << "Destructor called" << std::endl;
-}
+Fixed::~Fixed() {std::cout << "Destructor called" << std::endl;}
 
 Fixed &Fixed::operator=(const Fixed &fixed) {
-    std::cout << "Assignation operator called" << std::endl;
-    if (this != &fixed)
-        this->_rawBits = fixed.getRawBits();
-    return *this;
+	std::cout << "Assignation operator called" << std::endl;
+	if (this != &fixed)
+		this->_rawBits = fixed.getRawBits();
+	return *this;
 }
 
-float Fixed::toFloat(void) const {
-    return (float)this->_rawBits / (1 << this->_fractionalBits);
-}
+float Fixed::toFloat(void) const {return (float)this->_rawBits / (1 << this->_fractionalBits);}
 
-int Fixed::toInt(void) const {
-    return this->_rawBits >> this->_fractionalBits;
-}
+int Fixed::toInt(void) const {return this->_rawBits >> this->_fractionalBits;}
 
 int Fixed::getRawBits(void) const {
-    std::cout << "getRawBits member function called" << std::endl;
-    return this->_rawBits;
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->_rawBits;
 }
 
 void Fixed::setRawBits(int const raw) {
-    std::cout << "setRawBits member function called" << std::endl;
-    this->_rawBits = raw;
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_rawBits = raw;
 }
 
 std::ostream &operator<<(std::ostream &out, const Fixed &fixed) {
-    out << fixed.toFloat();
-    return out;
+	out << fixed.toFloat();
+	return out;
 }
--- Module_02/ex03/bsp.cpp
+++ Module_02/ex03/bsp.cpp
@@ -13,14 +13,12 @@ bool bsp(const Point &pt, const Point &v1, const Point &v2, const Point &v3) {
     d3 = sign(pt, v3, v1);
 
     // Debug infos
-    // std::cout << "v: " << sign(v1, v2, v3) << std::endl;
     // std::cout << "d1: " << d1 << std::endl;
     // std::cout << "d2: " << d2 << std::endl;
     // std::cout << "d3: " << d3 << std::endl;
-    // std::cout << "dTotal: " << d1 + d2 + d3 << std::endl;
 
-    hasNeg = (d1 < 0) || (d2 < 0) || (d3 < 0); // Check if the point is on the negative side of the plane
-    hasPos = (d1 > 0) || (d2 > 0) || (d3 > 0); // Check if the point is on the positive side of the plane
+    hasNeg = (d1 < 0) || (d2 < 0) || (d3 < 0);
+    hasPos = (d1 > 0) || (d2 > 0) || (d3 > 0);
 
-    return !(hasNeg && hasPos); // en fr : si le point est à la fois du côté négatif et positif d'un plan, il est à l'extérieur du triangle
+    return !(hasNeg && hasPos);
 }
